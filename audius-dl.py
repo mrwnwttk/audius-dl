c4ea0241c19c4e8fb417e5bc820380674490c9a0
mrwnwttk <me@mrwnwttk.xyz>
2020-10-24 19:56:55 +0200

    Fixes, a whole bunch of fixes

--- audius-dl.py
+++ audius-dl.py
@@ -1,18 +1,21 @@
 import re
 import os
+import io
 import sys
 import json
-import urllib
 import requests
 import urllib.parse
+import shutil
 from joblib import Parallel, delayed
 from mutagen.easyid3 import EasyID3
 from mutagen.mp4 import MP4, MP4Cover
 from mutagen.id3 import ID3, APIC, TIT2, TALB, TPE1, TPE2, COMM, USLT, TCOM, TCON, TDRC
+import subprocess
+
+import multiprocessing
 
 def fix_filename(filename):
 	return re.sub(r'[\/\*\<\?\>\|]', '-', filename)
-
 
 def get_artist_name(account):
 	username_json = requests.get("https://discoveryprovider3.audius.co/users?handle=" + account + "&limit=1&offset=0")
@@ -35,43 +38,49 @@ def add_tags(filename, title, artist, description, cover_flag):
 		tags.save(filename + ".m4a")
 
 def download_fragment(url, i):
-	print("\033[K", "Fragment: [{}/{}]".format(i, len(data['data'][0]['track_segments'])), "\r", end='')
+	global segments_arr
+	print("\033[K", "Fragment: [{}/{}]".format(i + 1, len(data['data'][0]['track_segments'])), "\r", end='')
 	sys.stdout.flush()
-	urllib.request.urlretrieve("https://creatornode.audius.co/ipfs/" + data['data'][0]['track_segments'][i]['multihash'], "dl/" + "{:04d}".format(i))
+	segments_arr[i] = requests.get("https://creatornode.audius.co/ipfs/" + data['data'][0]['track_segments'][i]['multihash']).content
 
 link = input("Please enter a link: ")
 
 link_array = link.split("/")
 account = link_array[3]
-track_id = link_array[4].split("-")
-track_id = track_id[-1]
-title = link_array[4]
-title = urllib.parse.unquote(title)
+track_id = (link_array[4].split("-"))[-1]
+
+title = urllib.parse.unquote(link_array[4])
 title = title [:-(len(track_id) + 1)]
+title = title.replace('"', '\"')
+title = urllib.parse.quote(title)
+#title = "%22mvinline-svn%22"
 
-curl_string = "curl --silent 'https://discoveryprovider3.audius.co/tracks_including_unlisted' -H 'authority: discoveryprovider3.audius.co' -H 'accept: application/json, text/plain, */*' -H 'origin: https://audius.co' -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36' -H 'content-type: application/json;charset=UTF-8' -H 'sec-fetch-site: same-site' -H 'sec-fetch-mode: cors' -H 'referer: " + link + "' -H 'accept-encoding: gzip, deflate, br' -H 'accept-language: en-US,en;q=0.9' --data-binary '{\"tracks\":[{\"id\":" + str(track_id) + ",\"url_title\":\""+ title + "\",\"handle\":\"" + account + "\"}]}' --compressed > dl.txt"
-os.system(curl_string)
-try:
-	os.mkdir("dl")
-except:
-	pass
+headers = {
+    'content-type': 'application/json;charset=UTF-8',
+    'referer': link,
+}
 
-with open("dl.txt") as f:
-	data = json.loads(f.read())
-	print("Number of fragments: {}".format(len(data['data'][0]['track_segments'])))
-	Parallel(n_jobs=16)(delayed(download_fragment)(data,i) for i in range(len(data['data'][0]['track_segments'])))
-	print("")
+data = '{"tracks":[{"id":' + str(track_id) + ',"url_title":"' + str(title.replace('"', '\"')) + '","handle":"' + str(account) + '"}]}'
+r = requests.post('https://discoveryprovider3.audius.co/tracks_including_unlisted', headers=headers, data=data)
 
-os.chdir("dl")
-os.system("cat * | ffmpeg -y -i pipe: -c:a copy \"{}.m4a\" -loglevel panic".format(track_id))
-os.system("mv \"{}.m4a\" ../\"{}.m4a\"".format(track_id, track_id))
-os.chdir("..")
-os.system("rm -rf dl/")
-os.system("rm dl.txt")
+data = json.loads(r.text)
+
+print("Number of fragments: {}".format(len(data['data'][0]['track_segments'])))
+manager = multiprocessing.Manager()
+segments_arr = manager.list([None] * len(data['data'][0]['track_segments']))
+
+Parallel(n_jobs=16)(delayed(download_fragment)(data,i) for i in range(len(data['data'][0]['track_segments'])))
+all_seg = b''.join(segments_arr)
+
+print("")
+p = subprocess.Popen(["ffmpeg", "-y", "-i", "pipe:", "-c:a", "copy", f"{track_id}.m4a"], stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.STDOUT)
+grep_stdout = p.communicate(input=all_seg)[0]
+
+#print(grep_stdout.decode())
 
 # Get cover and set cover flag
 try: 
-	urllib.request.urlretrieve("https://ipfs.io/ipfs/" + data['data'][0]['cover_art_sizes'] + "/1000x1000.jpg", "cover.jpg")
+	urllib.request.urlretrieve("https://creatornode.audius.co/ipfs/" + data['data'][0]['cover_art_sizes'] + "/1000x1000.jpg", "cover.jpg")
 	cover_flag = 1
 except:
 	cover_flag = 0
@@ -85,11 +94,13 @@ except:
 
 add_tags(track_id, data['data'][0]['title'], get_artist_name(account), description, 1)
 
-os.system("mv \"{}.m4a\" \"{}.m4a\"".format(track_id, fix_filename(data['data'][0]['title'])))
-
+#os.system("mv \"{}.m4a\" \"{}.m4a\"".format(track_id, fix_filename(data['data'][0]['title'])))
+shutil.move(f"{track_id}.m4a", f"{fix_filename(data['data'][0]['title'])}.m4a")
 try:
 	os.mkdir("Files")
 except:
 	pass
 os.system("mv *.m4a Files/")
 os.remove("cover.jpg")
+
+print("Done!")
